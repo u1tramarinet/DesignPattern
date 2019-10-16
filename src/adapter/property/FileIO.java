206ea676c3d3292778c5654cad61d3275aa38485
Suzuki Takumi <u1tramarine.takumi@gmail.com>
2019-10-16 23:47:45 +0900

    Fix FileIO of adapter.

--- src/adapter/property/FileIO.java
+++ src/adapter/property/FileIO.java
@@ -5,6 +5,6 @@ import java.io.IOException;
 public interface FileIO {
     void readFromFile(String filename) throws IOException;
     void writeToFile(String filename) throws IOException;
-    public void setValue(String key, String value);
-    public String getValue(String key);
+    void setValue(String key, String value);
+    String getValue(String key);
 }
