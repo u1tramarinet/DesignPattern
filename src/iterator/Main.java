409850804b22404a3c54039ba29f5ab7b6b4a5fd
Suzuki Takumi <u1tramarine.takumi@gmail.com>
2019-10-16 21:56:53 +0900

    Initial commit

---
+++ src/iterator/Main.java
@@ -0,0 +1,16 @@
+package iterator;
+
+public class Main {
+    public static void main(String[] args) {
+        BookShelf bookShelf = new BookShelf(4);
+        bookShelf.appendBook(new Book("book1"));
+        bookShelf.appendBook(new Book("book2"));
+        bookShelf.appendBook(new Book("book3"));
+        bookShelf.appendBook(new Book("book4"));
+        Iterator iterator = bookShelf.iterator();
+        while (iterator.hasNext()) {
+            Book book = (Book)iterator.next();
+            System.out.println(book.getName());
+        }
+    }
+}
