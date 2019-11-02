6143b9e0f1be0f9d1a998f78f93b0a276b9f379d
Suzuki Takumi <u1tramarine.takumi@gmail.com>
2019-11-02 23:25:20 +0900

    Modify Bridge pattern.

--- src/bridge/Main.java
+++ src/bridge/Main.java
@@ -6,9 +6,11 @@ public class Main {
         Display d1 = new Display(new StringDisplayImpl("Hello, Japan."));
         Display d2 = new CountDisplay(new StringDisplayImpl("Hello, World."));
         CountDisplay d3 = new CountDisplay(new StringDisplayImpl("Hello, Universe."));
+        RandomDisplay d4 = new RandomDisplay(new StringDisplayImpl("Hello, Random."));
         d1.display();
         d2.display();
         d3.display();
         d3.multiDisplay(5);
+        d4.randomDisplay(10);
     }
 }
